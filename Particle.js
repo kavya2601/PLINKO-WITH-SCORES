eb5ca7f7a79e55402986416f230b9075670f7888
kavya2601 <63140167+kavya2601@users.noreply.github.com>
2020-10-15 08:55:27 +0530

    Update Particle.js

--- Particle.js
+++ Particle.js
@@ -9,22 +9,35 @@ class Particle {
         this.body = Bodies.circle(x, y, this.r,options);       
         this.color=color(random(0, 255), random(0, 255), random(0, 255));
         World.add(world, this.body);
+        this.Visiblity = 255;
+    }  
+      display() {
+       if(this.body.position.y<750){
 
-    }
-    display() {
-
+       push();
         var pos = this.body.position;
         var angle = this.body.angle;
 
-        push();
         translate(pos.x, pos.y);
         rotate(angle);
+        
         //imageMode(CENTER);
+        
         noStroke();
         fill(this.color)
         ellipseMode(RADIUS);
         ellipse(0, 0, this.r,this.r);
+       pop();
+       }
+        else{
+        World.remove(world , this.body);
+        push();
+        this.Visiblity = this.Visiblity - 5;
+        tint(255,this.Visiblity);
         pop();
+        }
+    
     }
+
 
 };
